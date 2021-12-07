0137779e3915de1d1358bb3925c92c619aa5445a
ceddddy <ceddddyadvance@gmail.com>
2021-12-07 15:47:11 +0200

    first exersise

---
+++ app.js
@@ -0,0 +1,26 @@
+//EXERCISE 1, THE FORTUNE TELLER.
+
+//My variables
+
+let numOfChildren = 3;
+let spouse = "Cutie";
+let whereIStay = "Johannesburg";
+let proffession = "Web developer";
+
+//logging to the console
+
+console.log("You will be a " + proffession + " in " + whereIStay + " , and married to " + spouse + " with " + numOfChildren + " kids.");
+
+//outputting the sentence
+
+function showSentence() {
+
+}
+
+//_________________________________END____________________________________________________________________________________________________
+
+
+
+
+
+
