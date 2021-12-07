79bf531f6ba58bdf4950024db247f9904d401c1e
ceddddy <ceddddyadvance@gmail.com>
2021-12-07 16:10:50 +0200

    second exercise

---
+++ ageCalculator.js
@@ -0,0 +1,14 @@
+//EXERCISE 2 ,THE AGE CALCULATOR
+// My variables
+
+let birthYear = 1995;
+let futureYear = 2025;
+let firstPossibleAge = futureYear  - birthYear;
+let secondPossibleAge = futureYear - birthYear-1;
+
+//logging to the console
+
+console.log(firstPossibleAge);
+console.log(secondPossibleAge);
+
+//____________________END___________________________________
