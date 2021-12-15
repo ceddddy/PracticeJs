d3bb43842884723c2a3ea348f8e43ed7f94014db
ceddddy <ceddddyadvance@gmail.com>
2021-12-15 21:35:25 +0200

    The geometrizer

---
+++ scripts/geometrizer.js
@@ -0,0 +1,11 @@
+//My variables
+
+const radius = 25;
+const area = 3.14 * (radius*radius) ;
+const circumference = (radius*2) * 3.14;
+const circleCircumference = "The circumference is " + circumference + ".";
+
+//Logging to the console
+console.log(circleCircumference);
+console.log("The area is " + area);
+
