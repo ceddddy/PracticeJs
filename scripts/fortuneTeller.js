380d815d3c124cd39d67f1336ef7d30b79009a93
ceddddy <ceddddyadvance@gmail.com>
2021-12-27 16:56:29 +0200

    added fortune teller function

--- scripts/fortuneTeller.js
+++ scripts/fortuneTeller.js
@@ -13,14 +13,17 @@ console.log("You will be a " + proffession + " in " + whereIStay + " , and marri
 
 //outputting the sentence
 
-function showSentence() {
-
-}
-
 //_________________________________END____________________________________________________________________________________________________
 
+// the fortune teller function
+function tellFortune(jobTitle,whereYouStay,fianceName,numOfKids){
+    const fortune = "You will be a " + jobTitle + " in " + whereYouStay + " , and married to " + fianceName + " " + " with " + numOfKids + " kids.";
+    console.log(fortune);
+}
 
-
+tellFortune("developer","Johannesburg","Cutie",3);
+tellFortune("specialist","Mumbai","Rose",2);
+tellFortune("sportsman","Canada","Lila",1);
 
 
 
