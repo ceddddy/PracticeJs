20073b8ec11a17a7e6db62fd182557d77751f9c4
ceddddy <ceddddyadvance@gmail.com>
2022-01-03 00:49:05 +0200

    TEMPERATURE CONVERTER FUNCTIONS

--- scripts/tempConverter.js
+++ scripts/tempConverter.js
@@ -1,7 +1,33 @@
+// My variables
+
 const tempInCelcius = 25;
 const celciusToFahrenheit = (tempInCelcius * 9/5) + 32;
 const tempInFahrenheit = 90;
 const fahrenheitToCelsius = 5/9 * (tempInFahrenheit - 32);
 
+//Logging to console
+
 console.log("25°C is "+ celciusToFahrenheit + "°F");
 console.log("90°F is "+ fahrenheitToCelsius + "°C");
+
+// TEMPERATURE CONVERTER FUNCTIONS
+
+//celsius to fahrenheit function
+
+function celsiusToFahrenheit$(celsius) {
+
+const celciusToFahrenheit$$ = (celsius * 9/5) + 32;
+console.log( celsius + " °C is " + celciusToFahrenheit$$ + "°F");
+}
+
+celsiusToFahrenheit$(25);
+
+
+//fahrenheit to celcius function
+
+function fahrenheitToCelsius$$(fahrenheit) {
+  const fahrenheitToCelsius$ = 5/9 * (fahrenheit - 32);
+  console.log(fahrenheit + "°F is " + fahrenheitToCelsius$ + "°C");
+}
+
+fahrenheitToCelsius$$(90);
