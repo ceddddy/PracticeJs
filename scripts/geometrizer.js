b0c0740e127006ca4208453cf82b4dce3a07c8e4
ceddddy <ceddddyadvance@gmail.com>
2022-01-02 23:50:15 +0200

    added geometrizer functions

--- scripts/geometrizer.js
+++ scripts/geometrizer.js
@@ -8,4 +8,23 @@ const circleCircumference = "The circumference is " + circumference + ".";
 //Logging to the console
 console.log(circleCircumference);
 console.log("The area is " + area);
+//THE GEOMETRIZER FUNCTIONS
 
+//circumference function
+
+function calcCircumference(radius) {
+  const circumferenz = (radius*2) * Math.PI;
+  console.log("The circumference is " + circumferenz + ".");
+}
+
+calcCircumference(50);
+
+
+//area function
+
+function calcArea(radius) {
+  const area$ = Math.PI * (radius*radius) ;
+  console.log("The area is " + area$)
+}
+
+calcArea(50);
