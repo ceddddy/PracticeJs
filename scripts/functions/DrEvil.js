4c43ac044c6300db656aa41743e277e80ae8213d
ceddddy <ceddddyadvance@gmail.com>
2022-01-09 07:37:31 +0200

    added 2 functions(drevil and mixup)

---
+++ scripts/functions/DrEvil.js
@@ -0,0 +1,9 @@
+function DrEvil(amnt) {
+  if (amnt == 1000000) {
+    return amnt + " dollars  (pinky)";
+  } else {
+    return amnt + " dollars ";
+  }
+}
+
+DrEvil(5);
---
+++ scripts/functions/mixup.js
@@ -0,0 +1,5 @@
+function mixup(str1,str2) {
+  return str2.slice(0, 2) + str1.slice(2) + " " + str1.slice(0, 2) + str2.slice(2);
+}
+
+mixup("hi", "there");
