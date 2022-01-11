f35ef572bc3852c7780d29dc4397272ecb613412
ceddddy <ceddddyadvance@gmail.com>
2022-01-11 17:10:17 +0200

    edited evenOddReporter and created new file

--- scripts/forLoops/evenOddReporter.js
+++ scripts/forLoops/evenOddReporter.js
@@ -1,4 +1,4 @@
-for (var i = 0; i <= 20; i++) {    //i is set to 0; if i <= 20; keep adding 1 
+for (let i = 0; i <= 20; i++) {    //i is set to 0; if i <= 20; keep adding 1 
     if (i % 2 === 0) {             // if i modulus 2 is equal in value and in type to zero
         console.log(i + ' is even');    //log i + "is even"
     } else {
