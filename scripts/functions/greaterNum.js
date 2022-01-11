b36ea28fe0aa4689791d6458278a4d260f823ed9
ceddddy <ceddddyadvance@gmail.com>
2022-01-11 11:48:42 +0200

    added fixStart function,greaterNum function and modified mixup function

---
+++ scripts/functions/greaterNum.js
@@ -0,0 +1,26 @@
+function greaterNum (num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  }
+  else{
+  
+    return num2;
+  }
+}
+console.log(greaterNum(5, 4));
+console.log(greaterNum(6, 10));
+
+
+
+
+
+
+
+/*
+console.log(greaterNum("five", "fix"));
+//console.log(greaterNum(4, 2));
+
+const func1Num = greaterNum(5, 6);
+const func2Num = greaterNum(5, 2);
+
+console.log({func1Num, func2Num});*/
--- scripts/functions/mixup.js
+++ scripts/functions/mixup.js
@@ -2,4 +2,4 @@ function mixup(str1,str2) {
   return str2.slice(0, 2) + str1.slice(2) + " " + str1.slice(0, 2) + str2.slice(2);
 }
 
-mixup("hi", "there");
+console.log(mixup("hi", "there"));
