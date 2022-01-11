372500fd3b0f197953bc2f98d5965563034a6f9e
ceddddy <ceddddyadvance@gmail.com>
2022-01-11 13:59:15 +0200

    added forLoop folder and even and odd reporter

---
+++ scripts/forLoops/evenOddReporter.js
@@ -0,0 +1,8 @@
+for (var i = 0; i <= 20; i++) {    //i is set to 0; if i <= 20; keep adding 1 
+    if (i % 2 === 0) {             // if i modulus 2 is equal in value and in type to zero
+        console.log(i + ' is even');    //log i + "is even"
+    } else {
+        console.log(i + ' is odd');     //log i + "is odd"
+    }
+}
+
