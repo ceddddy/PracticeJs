d3d395e8d6da8510917b44a59a3abe00e3c9e494
ceddddy <ceddddyadvance@gmail.com>
2022-01-12 16:52:00 +0200

    added array folder and topChoice.js file

---
+++ scripts/arrays/topChoice.js
@@ -0,0 +1,9 @@
+const myChoices = ["green", "red", "blue"];
+myChoices.forEach(function(el, index) {
+  console.log(`My #${index + 1} choice is ${el}`);
+})
+/*
+for(let i = 0; i < myChoices.length; i++){
+  console.log(`My #${i + 1} is ${myChoices[i]}`);
+}*/
+
