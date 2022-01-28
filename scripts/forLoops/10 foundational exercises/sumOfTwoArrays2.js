81e140302d267f6254ed1bdb85629d83f3ab63e1
ceddddy <ceddddyadvance@gmail.com>
2022-01-28 23:50:04 +0200

    added sumOfTwoArrays file in 10 foundational exercises folder

---
+++ scripts/forLoops/10 foundational exercises/sumOfTwoArrays2.js
@@ -0,0 +1,12 @@
+let firstArray = [25, 40, 85];
+let secondArray = [50, 30, 10];
+
+let thirdArr = [];
+
+
+
+for (let i = 0; i < firstArray.length; i++) {
+  thirdArr[i] = firstArray[i] + secondArray[i];
+}
+
+console.log(thirdArr);
