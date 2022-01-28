b7250d9a64ecb52316461f34b7531760f59f826c
ceddddy <ceddddyadvance@gmail.com>
2022-01-29 01:52:00 +0200

    added uppercase.js in 10 foundational exercises folder

---
+++ scripts/forLoops/10 foundational exercises/uppercase.js
@@ -0,0 +1,8 @@
+let orgString = "javascript";
+
+let newString = orgString.split("")
+  .map((individualLetter, indexOfLetter) => (indexOfLetter % 2 != 0) ? individualLetter.toUpperCase() : individualLetter)
+  .join("");
+
+
+ console.log(newString);
