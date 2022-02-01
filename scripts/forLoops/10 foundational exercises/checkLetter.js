0c4d0d0650cc021fdd29caec949df9d8b0eb3786
ceddddy <ceddddyadvance@gmail.com>
2022-02-02 01:49:51 +0200

    updated checkletter.js and factorial.js in 10 foundational

--- scripts/forLoops/10 foundational exercises/checkLetter.js
+++ scripts/forLoops/10 foundational exercises/checkLetter.js
@@ -7,10 +7,67 @@
 //     console.log("No");
 //   }
 // }
-function containsLetter(str) {
-  (str.includes("y")) ? console.log("Yes") : console.log("No");
+
+
+
+// function containsLetter(str) {
+//   (str.includes("y")) ? console.log("Yes") : console.log("No");
+// }
+
+// containsLetter("don’t know why");
+// containsLetter("do it all");
+// containsLetter("DON'T KNOW WHY");
+
+
+
+
+// function containsLetter (str) {
+//   const myString = str.split("");
+//   for (el of myString) {
+//     if (el === "y") {
+//       console.log("Yes");
+//       break;
+//     } else {
+//       console.log("No");
+//       continue;
+//     }
+//   } 
+
+// }
+
+// containsLetter("don’t know why");
+// containsLetter("do it all");
+
+function withLetter(str) {
+  let arrOfStr = str.split("");  //change string to array
+  
+  let hasY = "No";// default value return to no
+  for (i = 0; i < arrOfStr.length; i++) {  //looping through the array
+    if (arrOfStr[i] === "y") { //checking every index [i] of the array if it sustains the given condition 
+      hasY = "Yes";  // updates y to yes
+      break; //breaking the loop once the condition returns true/trminates the loop
+    } 
+  }
+  return hasY; // return the value of hasY
 }
 
-containsLetter("don’t know why");
-containsLetter("do it all");
-containsLetter("DON'T KNOW WHY");
+console.log(withLetter("don’t know why"));  //logging some test cases
+console.log(withLetter("the letter isnt here"));
+
+// const withLetter = (str) => {
+//   const arrOfStr = str.split('');
+
+//   let hasY = 'No'; //defaults return to No
+
+//   for(let i = 0; i < str.length; i++){
+//       if(arrOfStr[i] === 'y'){
+//           hasY = 'Yes'; // updates y to yes
+//           break; // terminates the loop 
+//       }
+//   }
+
+//   return hasY; // return the value of hasY
+// }
+
+// console.log(withLetter('I dont know why'));
+// console.log(withLetter('the letter isnt here'));
--- scripts/forLoops/10 foundational exercises/factorial.js
+++ scripts/forLoops/10 foundational exercises/factorial.js
@@ -1,16 +1,30 @@
-//Given a number n Calculate the factorial of the number
-const factorial = (n) => {
-  if (n < 0) {
-    throw 'factorial error on a negative number!';
+// //Given a number n Calculate the factorial of the number
+// const factorial = (n) => {
+//   if (n < 0) {
+//     throw 'factorial error on a negative number!';
+//   }
+//   let r = 1;
+//   while (n) {
+//     r = r * n--;
+//   }
+//   console.log(r);
+//   return r;
+// }
+
+// factorial(7);
+// factorial(100);
+// factorial(170);
+
+
+
+function fact(number) {
+  let factorial = 1;
+  for (i = number; i > 0; i--) {
+    factorial *= i;
+    console.log(factorial);
+    //console.log(i);
   }
-  let r = 1;
-  while (n) {
-    r = r * n--;
-  }
-  console.log(r);
-  return r;
 }
 
-factorial(7);
-factorial(100);
-factorial(170);
+fact(7);
+
