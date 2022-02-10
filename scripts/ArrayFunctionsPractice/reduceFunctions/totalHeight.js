a47fcef8641063ab5929355db8763c2367dbf11c
ceddddy <ceddddyadvance@gmail.com>
2022-02-11 01:58:53 +0200

    added totalHeight.js in reduceFunctions

---
+++ scripts/ArrayFunctionsPractice/reduceFunctions/totalHeight.js
@@ -0,0 +1,44 @@
+
+const characters = [{
+  name: 'Luke Skywalker',
+  height: '172',
+  mass: '77',
+  eye_color: 'blue',
+  gender: 'male',
+},
+{
+  name: 'Darth Vader',
+  height: '202',
+  mass: '136',
+  eye_color: 'yellow',
+  gender: 'male',
+},
+{
+  name: 'Leia Organa',
+  height: '150',
+  mass: '49',
+  eye_color: 'brown',
+  gender: 'female',
+},
+{
+  name: 'Anakin Skywalker',
+  height: '188',
+  mass: '84',
+  eye_color: 'blue',
+  gender: 'male',
+},
+];
+
+//get total height of all characters
+
+// const totalHeight = (preVal, currVal) => {
+//   return preVal + +currVal.height;
+// }
+
+// console.log(characters.reduce(totalHeight, 0));
+
+const totalHeight = characters.reduce((preVal, currVal) => {
+  return preVal + +currVal.height;
+}, 0);
+
+console.log(totalHeight);
