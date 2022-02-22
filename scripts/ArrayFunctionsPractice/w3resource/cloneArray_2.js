96e06d7228eb58b1e20741541e7a308f650cfc1e
ceddddy <ceddddyadvance@gmail.com>
2022-02-22 15:41:40 +0200

    edited cloneArray_2

--- scripts/ArrayFunctionsPractice/w3resource/cloneArray_2.js
+++ scripts/ArrayFunctionsPractice/w3resource/cloneArray_2.js
@@ -1,7 +1,9 @@
 //clonning an array
 
 const cloneArray = (array) => {
-return array.slice(-3, -1);
+return array.slice(0);
 }
 
 console.log(cloneArray([1, 2, 3, 4]));
+console.log(cloneArray([1, 2, 4, 0]));
+console.log(cloneArray([1 ,2, [4, 0]]));
