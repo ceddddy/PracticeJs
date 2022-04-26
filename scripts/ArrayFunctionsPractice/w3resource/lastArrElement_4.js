e76820239e624e62c518f826bb061046e2d46bd7
Ceddy <ceddddyadvance@gmail.com>
2022-04-26 03:30:32 +0200

    added comments in lastArrElement file

--- scripts/ArrayFunctionsPractice/w3resource/lastArrElement_4.js
+++ scripts/ArrayFunctionsPractice/w3resource/lastArrElement_4.js
@@ -7,16 +7,18 @@ const lastArrElement = (arr, n) => {
     return void 0;
   }
   if (n == null) {
-    
     return arr[arr.length - 1];
   }
   // console.log(arr[arr.length-1]);
+  // The slice() method returns selected elements in an array, as a new array.
+  // The slice() method selects from a given start, up to a (not inclusive) given end.
+  // Syntax :array.slice(start, end)
   return arr.slice(Math.max(arr.length - n, 0));
 };
 
 console.log(lastArrElement([7, 9, 0, -2]));
-console.log(lastArrElement([7, 9, 0, -2],3));
-console.log(lastArrElement([7, 9, 0, -2],6));
+console.log(lastArrElement([7, 9, 0, -2], 3));
+console.log(lastArrElement([7, 9, 0, -2], 6));
 // Expected Output :
 // -2
 // [9, 0, -2]
